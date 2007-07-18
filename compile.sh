abab2accf753467b2dbb5fadc4070f6935a187ba
Michalis Kamburelis <michalis.kambi@gmail.com>
2007-07-18 15:44:48 +0000

    + compile.sh scripts added to SVN to comfortably compile all * svn:ignore set on many dirs to ignore executables created there by compile.sh scripts
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/view3dscene@1813 afe8dcc5-9094-451b-8773-5f495042a737

---
+++ compile.sh
@@ -0,0 +1,28 @@
+#! /bin/sh
+set -eu
+
+# This is automatically generated script that should compile
+# all programs in this archive. It simply calls FPC
+# with proper command-line options.
+#
+# We must do cd ../kambi_vrml_game_engine/ (and call FPC from that directory)
+# because kambi.cfg file is there and it contains paths relative
+# to that directory.
+
+cd ../kambi_vrml_game_engine/
+
+# This program uses GLWindow unit. GLWindow unit may be compiled
+# with various back-ends (e.g. under Unices two most useful back-ends
+# are XLIB and GTK). To make sure that compilation of this program
+# will produce exactly what you need, below we make sure that
+# unit GLWindow will be *always* *rebuild*.
+#
+# Of course this means that compilation time will suffer a little,
+# since GLWindow unit will be possibly rebuild without any real need.
+# Comment out line below if you want.
+rm -f opengl/glwindow.o \
+      opengl/glwindow.ppu \
+      opengl/GLWindow.o \
+      opengl/GLWindow.ppu
+
+fpc -dRELEASE @kambi.cfg ../view3dscene/view3dscene.dpr
