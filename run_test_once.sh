93a21cf504b1bf5b730efa174a553eefa6db450a
Michalis Kamburelis <michalis.kambi@gmail.com>
2008-06-04 17:19:22 +0000

    * view3dscene run_tests script improved:   * it compares header lines of input and output, to see if VRML version generated is the same   * optionally disables heaptrc
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/view3dscene@2872 afe8dcc5-9094-451b-8773-5f495042a737

--- run_test_once.sh
+++ run_test_once.sh
@@ -3,6 +3,9 @@ set -eu
 
 # This is usually run by run_tests.sh script.
 
+# You can disable heaptrc for shorter output (useful only for debug builds
+# that have heaptrc compiled in)
+export HEAPTRC=disabled
 
 FILE="$1"
 
@@ -14,6 +17,31 @@ TEMP_FILE=`dirname "$FILE"`/view3dscene_test_temporary_file.wrl
 echo '---- Reading' "$FILE"
 echo '(temp is ' "$TEMP_FILE" ')'
 view3dscene "$FILE" --write-to-vrml > "$TEMP_FILE"
+
+# Check input file and output file headers.
+# They indicate VRML version used to write the file.
+# They should match, otherwise SuggestVRMLVersion of some nodes
+# possibly doesn't work and the resulting file has different VRML version.
+#
+# Note that this test works only with classic VRML files (XML X3D versions,
+# or gzip compressed, have version elsewhere, other 3D formats are converted
+# to any version we like, and output is always classic VRML --- so comparison
+# has no sense).
+
+FILE_EXTENSION=`stringoper ExtractFileExt "$FILE"`
+
+if [ '(' '(' "$FILE_EXTENSION" = '.wrl' ')' -o \
+         '(' "$FILE_EXTENSION" = '..x3dv' ')' ')' -a \
+     '(' `basename "$FILE"` != cones_gzipped_but_with_normal_extension.wrl ')' ]; then
+  INPUT_HEADER=`head -n 1 "$FILE"`
+  OUTPUT_HEADER=`head -n 1 "$TEMP_FILE"`
+  if [ "$INPUT_HEADER" != "$OUTPUT_HEADER" ]; then
+    echo 'WARNING: input/output headers differ:'
+    echo 'Header on input is' "$INPUT_HEADER"
+    echo 'Header on output is' "$OUTPUT_HEADER"
+  fi
+fi
+
 echo '---- Reading again' "$FILE"
 view3dscene "$TEMP_FILE" --write-to-vrml > /dev/null
 
