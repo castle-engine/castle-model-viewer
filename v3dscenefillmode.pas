1679a67bc3341f15e2514fb98dee81faaaaaa5c3
Michalis Kamburelis <michalis.kambi@gmail.com>
2008-11-13 06:10:01 +0000

    * updated docs (www and vrml_engine_doc) about shadows:   * mention view3dscene "Silhouette and Border Edges", as this is a very useful tool for people playing with shadows   * mention shadows_dynamic.x3dv   * mention models must be 2-manifold * view3dscene menu items fixed (To Be In Capatalized Letters)
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/view3dscene@3764 afe8dcc5-9094-451b-8773-5f495042a737

--- v3dscenefillmode.pas
+++ v3dscenefillmode.pas
@@ -20,15 +20,15 @@ const
     WireframeEffect is generally not costly. }
   ( (Name: 'Normal'                               ; PureGeometry: false; WireframeEffect: weNormal        ; WireframeColor: (0, 0, 0); BackgroundWireframe: false; ),
     (Name: 'Wireframe'                            ; PureGeometry: false; WireframeEffect: weWireframeOnly ; WireframeColor: (0, 0, 0); BackgroundWireframe: true ; ),
-    (Name: 'Solid wireframe'                      ; PureGeometry: false; WireframeEffect: weSolidWireframe; WireframeColor: (1, 1, 1); BackgroundWireframe: false; ),
-    (Name: 'Normal with silhouette'               ; PureGeometry: false; WireframeEffect: weSilhouette    ; WireframeColor: (1, 1, 1); BackgroundWireframe: false; ),
+    (Name: 'Solid Wireframe'                      ; PureGeometry: false; WireframeEffect: weSolidWireframe; WireframeColor: (1, 1, 1); BackgroundWireframe: false; ),
+    (Name: 'Normal with Silhouette'               ; PureGeometry: false; WireframeEffect: weSilhouette    ; WireframeColor: (1, 1, 1); BackgroundWireframe: false; ),
 
-    (Name: 'Solid shape'                          ; PureGeometry: true ; WireframeEffect: weNormal        ; WireframeColor: (0, 0, 0); BackgroundWireframe: false; ),
-    (Name: 'Wireframe (single color)'             ; PureGeometry: true ; WireframeEffect: weWireframeOnly ; WireframeColor: (1, 1, 1); BackgroundWireframe: true ; ),
-    (Name: 'Solid wireframe (single color)'       ; PureGeometry: true ; WireframeEffect: weSolidWireframe; WireframeColor: (0, 0, 0); BackgroundWireframe: false; ),
-    (Name: 'Normal with silhouette (single color)'; PureGeometry: true ; WireframeEffect: weSilhouette    ; WireframeColor: (0, 0, 0); BackgroundWireframe: false; ),
+    (Name: 'Solid Shape'                          ; PureGeometry: true ; WireframeEffect: weNormal        ; WireframeColor: (0, 0, 0); BackgroundWireframe: false; ),
+    (Name: 'Wireframe (Single Color)'             ; PureGeometry: true ; WireframeEffect: weWireframeOnly ; WireframeColor: (1, 1, 1); BackgroundWireframe: true ; ),
+    (Name: 'Solid Wireframe (Single Color)'       ; PureGeometry: true ; WireframeEffect: weSolidWireframe; WireframeColor: (0, 0, 0); BackgroundWireframe: false; ),
+    (Name: 'Normal with Silhouette (Single Color)'; PureGeometry: true ; WireframeEffect: weSilhouette    ; WireframeColor: (0, 0, 0); BackgroundWireframe: false; ),
 
-    (Name: 'Silhouette and border edges'          ;
+    (Name: 'Silhouette and Border Edges'          ;
       { PureGeometry, WireframeEffect, WireframeColor don't matter for this }
       PureGeometry: false; WireframeEffect: weNormal        ; WireframeColor: (0, 0, 0);
       BackgroundWireframe: false)
