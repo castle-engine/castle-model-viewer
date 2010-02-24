7e2b52422d14ea9ee1c45d192dc099e3710054c6
Michalis Kamburelis <michalis.kambi@gmail.com>
2010-02-24 06:11:59 +0000

    * update (~/bin/update_copyright_year) all copyright preambles to include -2010 year and ----------- separator line
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/view3dscene@5528 afe8dcc5-9094-451b-8773-5f495042a737

--- bgcolors.pas
+++ bgcolors.pas
@@ -1,5 +1,5 @@
 {
-  Copyright 2002-2005 Michalis Kamburelis.
+  Copyright 2002-2010 Michalis Kamburelis.
 
   This file is part of "view3dscene".
 
@@ -16,6 +16,8 @@
   You should have received a copy of the GNU General Public License
   along with "view3dscene"; if not, write to the Free Software
   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+  ----------------------------------------------------------------------------
 }
 
 { Simple management of glClearColor OpenGL property. }
--- texturefilters.pas
+++ texturefilters.pas
@@ -1,5 +1,5 @@
 {
-  Copyright 2003-2004 Michalis Kamburelis.
+  Copyright 2003-2010 Michalis Kamburelis.
 
   This file is part of "view3dscene".
 
@@ -16,6 +16,8 @@
   You should have received a copy of the GNU General Public License
   along with "view3dscene"; if not, write to the Free Software
   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+  ----------------------------------------------------------------------------
 }
 
 
