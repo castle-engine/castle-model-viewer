4230c2482a2436ef1e6885b523836438f78ac9ee
Michalis Kamburelis <michalis.kambi@gmail.com>
2011-07-12 23:50:07 +0000

    Add ability to compare --write-to-vrml results.
    Also move screenshot comparison code to local function.
    
    
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/view3dscene@9457 afe8dcc5-9094-451b-8773-5f495042a737

--- run_test_once.sh
+++ run_test_once.sh
@@ -19,8 +19,8 @@ fi
 # this will be used to make a reference (considered "correct") screenshot,
 # that will be compared with others. It may be just the same view3dscene
 # version, or it may be some other stable version.
-VIEW3DSCENE_FOR_CORRECT_SCREENSHOT="$VIEW3DSCENE"
-#VIEW3DSCENE_FOR_CORRECT_SCREENSHOT=view3dscene-3.8.0-release
+#VIEW3DSCENE_FOR_CORRECT_SCREENSHOT="$VIEW3DSCENE"
+VIEW3DSCENE_FOR_CORRECT_SCREENSHOT=view3dscene-3.10.0-release
 #VIEW3DSCENE_FOR_CORRECT_SCREENSHOT="$HOME"/sources/vrmlengine/view3dscene-old-renderer-for-comparison/view3dscene/view3dscene
 
 FILE="$1"
@@ -32,7 +32,7 @@ TEMP_FILE=`dirname "$FILE"`/view3dscene_test_temporary_file.wrl
 
 echo '---- Reading' "$FILE"
 #echo '(temp is ' "$TEMP_FILE" ')'
-"$VIEW3DSCENE" "$FILE" --write-to-vrml > "$TEMP_FILE"
+"$VIEW3DSCENE" "$FILE" --write --encoding=classic > "$TEMP_FILE"
 
 # Check input file and output file headers.
 # They indicate VRML version used to write the file.
@@ -66,48 +66,79 @@ if [ '(' '(' "$FILE_EXTENSION" = '.wrl' ')' -o \
 fi
 
 echo '---- Reading again' "$FILE"
-"$VIEW3DSCENE" "$TEMP_FILE" --write-to-vrml > /dev/null
+"$VIEW3DSCENE" "$TEMP_FILE" --write --encoding=classic > /dev/null
 
 rm -f "$TEMP_FILE"
 
-# Screenshot comparison ------------------------------------------------------
+# SaveToStream comparison ----------------------------------------------------
 
-# Comment this "return" line, to have much longer test,
-# doing --screenshot and comparing results. This tests the renderer.
-exit 0
-
-SCREENSHOT_CORRECT=`stringoper ChangeFileExt "$FILE" _test_screen.png`
-SCREENSHOT_COMPARE=`stringoper ChangeFileExt "$FILE" _test_screen_compare.png`
-
-mk_screnshot ()
+do_compare_classic_save ()
 {
-  echo '---- Rendering and making screenshot' "$VIEW3DSCENE_FOR_CORRECT_SCREENSHOT"
-  "$VIEW3DSCENE_FOR_CORRECT_SCREENSHOT" "$FILE" --screenshot 0 --geometry 300x200 "$SCREENSHOT_CORRECT"
+  SAVE_CLASSIC_OLD=`stringoper ChangeFileExt "$FILE" _classic_save_test_old.x3dv`
+  SAVE_CLASSIC_NEW=`stringoper ChangeFileExt "$FILE" _classic_save_test_new.x3dv`
+
+  echo '---- Comparing classic save with' "$VIEW3DSCENE_FOR_CORRECT_SCREENSHOT"
+  "$VIEW3DSCENE_FOR_CORRECT_SCREENSHOT" "$FILE" --write-to-vrml > "$SAVE_CLASSIC_OLD"
+  "$VIEW3DSCENE"                        "$FILE" --write-to-vrml > "$SAVE_CLASSIC_NEW"
+  diff -wur "$SAVE_CLASSIC_OLD" "$SAVE_CLASSIC_NEW"
+
+  rm -f "$SAVE_CLASSIC_OLD" "$SAVE_CLASSIC_NEW"
 }
 
-compare_screenshot ()
-{
-  echo '---- Comparing screenshot' "$VIEW3DSCENE"
-  "$VIEW3DSCENE" "$FILE" --screenshot 0 --geometry 300x200 "$SCREENSHOT_COMPARE"
+# Uncomment this to compare classic save with other (older?) view3dscene version.
+# Uses --write (--write-to-vrml for older view3dscene versions) to save,
+# and standard Unix "diff" to compare.
+# do_compare_classic_save
 
-  # Don't exit of screenshot comparison fail. That's because
-  # taking screenshots takes a long time, so just continue checking.
-  # The caller will have to check script output to know if something failed.
-  # (Also the comparison images will be left, unrecognized
-  # by version control system, for cases that failed.)
-  if image_compare "$SCREENSHOT_CORRECT" "$SCREENSHOT_COMPARE"; then
-    true # do nothing
-  else
-    DELETE_SCREENSHOTS=''
+# Screenshot comparison ------------------------------------------------------
+
+do_compare_screenshot ()
+{
+  SCREENSHOT_CORRECT=`stringoper ChangeFileExt "$FILE" _test_screen.png`
+  SCREENSHOT_COMPARE=`stringoper ChangeFileExt "$FILE" _test_screen_compare.png`
+
+  mk_screnshot ()
+  {
+    echo '---- Rendering and making screenshot' "$VIEW3DSCENE_FOR_CORRECT_SCREENSHOT"
+    "$VIEW3DSCENE_FOR_CORRECT_SCREENSHOT" "$FILE" --screenshot 0 --geometry 300x200 "$SCREENSHOT_CORRECT"
+  }
+
+  compare_screenshot ()
+  {
+    echo '---- Comparing screenshot' "$VIEW3DSCENE"
+    "$VIEW3DSCENE" "$FILE" --screenshot 0 --geometry 300x200 "$SCREENSHOT_COMPARE"
+
+    # Don't exit of screenshot comparison fail. That's because
+    # taking screenshots takes a long time, so just continue checking.
+    # The caller will have to check script output to know if something failed.
+    # (Also the comparison images will be left, unrecognized
+    # by version control system, for cases that failed.)
+    if image_compare "$SCREENSHOT_CORRECT" "$SCREENSHOT_COMPARE"; then
+      true # do nothing
+    else
+      DELETE_SCREENSHOTS=''
+    fi
+  }
+
+  DELETE_SCREENSHOTS='t'
+
+  mk_screnshot
+  compare_screenshot
+
+  if [ -n "$DELETE_SCREENSHOTS" ]; then
+    rm -f "$SCREENSHOT_CORRECT"
+    rm -f "$SCREENSHOT_COMPARE"
   fi
 }
 
-DELETE_SCREENSHOTS='t'
-
-mk_screnshot
-compare_screenshot
-
-if [ -n "$DELETE_SCREENSHOTS" ]; then
-  rm -f "$SCREENSHOT_CORRECT"
-  rm -f "$SCREENSHOT_COMPARE"
-fi
+# Uncomment this to compare screenshots with other (older?) view3dscene version.
+# Uses --screenshot to capture, and image_compare
+# (see kambi_vrml_game_engine/examples/images/) to compare.
+# This is quite longer test, but it nicely checks for any regressions
+# in the renderer.
+#
+# The output (_test_screen*.png files that were not removed) should be examined
+# by hand afterwards. Note that many differences should be ignored
+# (OpenGL is not guaranteed to display the same scene pixel-by-pixel exactly
+# the same if some state is different).
+# do_compare_screenshot
