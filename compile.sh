761347887b190d4f7dca8a5804c85848a9aeaed2
Michalis Kamburelis <michalis.kambi@gmail.com>
2011-10-05 02:53:46 +0000

    Rename kambi.cfg -> castle-fpc.cfg, KAMBI_FPC_OPTIONS -> CASTLE_FPC_OPTIONS, fix comments in many xxx_compile.sh scripts.
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/view3dscene@10168 afe8dcc5-9094-451b-8773-5f495042a737

--- compile.sh
+++ compile.sh
@@ -5,7 +5,7 @@ set -eu
 # and related programs here.
 #
 # We must do cd ../castle_game_engine/ (and call FPC from that directory)
-# because kambi.cfg file is there and it contains paths relative
+# because castle-fpc.cfg file is there and it contains paths relative
 # to that directory.
 
 cd ../castle_game_engine/
@@ -13,5 +13,5 @@ cd ../castle_game_engine/
 # Force rebuilding CastleWindow unit with proper backend.
 make clean-window
 
-fpc -dRELEASE "${KAMBI_FPC_OPTIONS:-}" @kambi.cfg ../view3dscene/view3dscene.lpr
-fpc -dRELEASE "${KAMBI_FPC_OPTIONS:-}" @kambi.cfg ../view3dscene/tovrmlx3d.lpr
+fpc -dRELEASE "${CASTLE_FPC_OPTIONS:-}" @castle-fpc.cfg ../view3dscene/view3dscene.lpr
+fpc -dRELEASE "${CASTLE_FPC_OPTIONS:-}" @castle-fpc.cfg ../view3dscene/tovrmlx3d.lpr
