98f79f91ce55d5e61c802b1cef53e626ef8d9c06
Michalis Kamburelis <michalis.kambi@gmail.com>
2013-07-27 12:30:28 +0000

    Make view3dscene desktop scripts a little easier to reuse, by APP_NAME variable.
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/view3dscene@12950 afe8dcc5-9094-451b-8773-5f495042a737

--- desktop/install.sh
+++ desktop/install.sh
@@ -4,6 +4,8 @@ set -eu
 # Install view3dscene menu entries, icons, mime types (to open view3dscene
 # automatically when clicking on 3D models).
 
+APP_NAME='view3dscene'
+
 # For non-user install, change this to
 #   SHARE_PREFIX = /usr/local/share
 # For Debian package, you probably want to set this to
@@ -12,17 +14,17 @@ SHARE_PREFIX="$HOME"/.local/share
 
 # Install mime types:
 mkdir -p "$SHARE_PREFIX"/mime/packages/
-cp view3dscene.xml "$SHARE_PREFIX"/mime/packages/
+cp -f "$APP_NAME".xml "$SHARE_PREFIX"/mime/packages/
 update-mime-database "$SHARE_PREFIX"/mime
 
 # Install icons:
 mkdir -p "$SHARE_PREFIX"/icons/hicolor/scalable/apps/
-cp view3dscene.svg "$SHARE_PREFIX"/icons/hicolor/scalable/apps/view3dscene.svg
+cp -f "$APP_NAME".svg "$SHARE_PREFIX"/icons/hicolor/scalable/apps/
 
 # Install also 48x48 PNG version, this helps nautilus to display icon
 # properly on the desktop.
 mkdir -p "$SHARE_PREFIX"/icons/hicolor/48x48/apps/
-cp view3dscene.png "$SHARE_PREFIX"/icons/hicolor/48x48/apps/view3dscene.png
+cp -f "$APP_NAME".png "$SHARE_PREFIX"/icons/hicolor/48x48/apps/
 
 # Is this needed?
 #if which update-icon-caches >/dev/null 2>&1 ; then update-icon-caches "$SHARE_PREFIX"/icons/hicolor/; fi
@@ -32,7 +34,7 @@ cp view3dscene.png "$SHARE_PREFIX"/icons/hicolor/48x48/apps/view3dscene.png
 # I don't know for sure what update-desktop-database actually does,
 # it's better to be safe).
 mkdir -p "$SHARE_PREFIX"/applications/
-cp view3dscene.desktop "$SHARE_PREFIX"/applications/
+cp -f "$APP_NAME".desktop "$SHARE_PREFIX"/applications/
 if which update-desktop-database >/dev/null 2>&1 ; then update-desktop-database -q "$SHARE_PREFIX"/applications/; fi
 
-echo 'Installed view3dscene (menu entries, icons, mime types): all OK.'
+echo "Installed $APP_NAME (menu entries, icons, mime types): all OK."
