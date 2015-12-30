202fea7e33195ac83a1c25290912b75c903d46c3
Michalis Kamburelis <michalis.kambi@gmail.com>
2015-12-30 23:57:18 +0000

    Allow compiling tovrmlx3d with castle-engine tool.
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/view3dscene@15018 afe8dcc5-9094-451b-8773-5f495042a737

--- compile.sh
+++ compile.sh
@@ -1,30 +1,35 @@
 #! /bin/sh
 set -eu
 
-# Call FPC with proper command-line options to compile view3dscene
-# and related programs here.
-#
-# We must do cd ../castle_game_engine/ (and call FPC from that directory)
-# because castle-fpc.cfg file is there and it contains paths relative
-# to that directory.
-
-cd ../castle_game_engine/
+# Compile view3dscene and tovrmlx3d.
 
 # Force rebuilding CastleWindow unit with proper backend.
+cd ../castle_game_engine/
 make --quiet clean-window
+cd ../view3dscene/
 
 # fpc -dRELEASE ${CASTLE_FPC_OPTIONS:-} @castle-fpc.cfg ../view3dscene/code/view3dscene.lpr
 # Compile view3dscene binary using castle-engine,
 # this is good for Windows to include the icon/versioninfo/manfest in resources.
-cd ../view3dscene/
 castle-engine compile ${CASTLE_ENGINE_TOOL_OPTIONS:-}
-cd ../castle_game_engine/
 
-fpc -dRELEASE ${CASTLE_FPC_OPTIONS:-} @castle-fpc.cfg ../view3dscene/code/tovrmlx3d.lpr
-# move tovrmlx3d binaries up
-if [ -f ../view3dscene/code/tovrmlx3d ]; then
-  mv -f ../view3dscene/code/tovrmlx3d ../view3dscene/
+if which castle-engine  > /dev/null; then
+  cd code/
+  castle-engine simple-compile ${CASTLE_ENGINE_TOOL_OPTIONS:-} tovrmlx3d.lpr
+  cd ../
+else
+  # We must do cd ../castle_game_engine/ (and call FPC from that directory)
+  # because castle-fpc.cfg file is there and it contains paths relative
+  # to that directory.
+  cd ../castle_game_engine/
+  fpc -dRELEASE ${CASTLE_FPC_OPTIONS:-} @castle-fpc.cfg ../view3dscene/code/tovrmlx3d.lpr
+  cd ../view3dscene/
 fi
-if [ -f ../view3dscene/code/tovrmlx3d.exe ]; then
-  mv -f ../view3dscene/code/tovrmlx3d.exe ../view3dscene/
+
+# move tovrmlx3d binaries up
+if [ -f code/tovrmlx3d ]; then
+  mv -f code/tovrmlx3d .
+fi
+if [ -f code/tovrmlx3d.exe ]; then
+  mv -f code/tovrmlx3d.exe .
 fi
