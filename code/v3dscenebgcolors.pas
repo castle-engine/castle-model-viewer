0ab5610f2879bda498ac67d399c4f15d52f99b3a
Michalis Kamburelis <michalis.kambi@gmail.com>
2016-02-03 05:56:26 +0000

    Fix compilation.
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/view3dscene@15100 afe8dcc5-9094-451b-8773-5f495042a737

--- code/v3dscenebgcolors.pas
+++ code/v3dscenebgcolors.pas
@@ -65,13 +65,13 @@ type
 
 class procedure TConfigOptions.LoadFromConfig(const Config: TCastleConfig);
 begin
-  BGColor := Config.GetVector(
+  BGColor := Config.GetVector4(
     'video_options/default_background_color', DefaultBGColor);
 end;
 
 class procedure TConfigOptions.SaveToConfig(const Config: TCastleConfig);
 begin
-  Config.SetDeleteVector('video_options/default_background_color',
+  Config.SetDeleteVector4('video_options/default_background_color',
     BGColor, DefaultBGColor);
 end;
 
