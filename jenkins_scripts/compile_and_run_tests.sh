86df0dbd2488ac87442dd4254fcf3a9612905a7f
Michalis Kamburelis <michalis.kambi@gmail.com>
2017-01-22 10:41:44 +0100

    Replace `pwd` with DIR in test output

--- jenkins_scripts/compile_and_run_tests.sh
+++ jenkins_scripts/compile_and_run_tests.sh
@@ -8,5 +8,8 @@ cd ../
 jenkins_scripts/run_tests.sh /tmp/view3dscene_run_tests_output.txt /tmp/view3dscene_run_tests_output_verbose.txt
 # remove OpenAL trash from outpt
 sed --in-place=.bak -e '/ALSA lib/d' -e '/AL lib/d' /tmp/view3dscene_run_tests_output.txt
+# replace current dir (present in some output messages) with string "DIR",
+# to make the result reproducible, regardless of the current directory.
+sed --in-place=.bak2 -e 's|`pwd`|DIR|' /tmp/view3dscene_run_tests_output.txt
 diff -u /tmp/view3dscene_run_tests_output.txt jenkins_scripts/run_tests_valid_output.txt
 rm -f /tmp/view3dscene_run_tests_output.txt
