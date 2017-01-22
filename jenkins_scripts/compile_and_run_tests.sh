c940d71e598244438e4702b419bb969ff2dace6f
Michalis Kamburelis <michalis.kambi@gmail.com>
2017-01-22 10:47:22 +0100

    Take upper dir for replacement, not view3dscene dir

--- jenkins_scripts/compile_and_run_tests.sh
+++ jenkins_scripts/compile_and_run_tests.sh
@@ -6,10 +6,16 @@ cd ../
 
 ./compile.sh
 jenkins_scripts/run_tests.sh /tmp/view3dscene_run_tests_output.txt /tmp/view3dscene_run_tests_output_verbose.txt
+
 # remove OpenAL trash from outpt
 sed --in-place=.bak -e '/ALSA lib/d' -e '/AL lib/d' /tmp/view3dscene_run_tests_output.txt
+
 # replace current dir (present in some output messages) with string "DIR",
 # to make the result reproducible, regardless of the current directory.
-sed --in-place=.bak2 -e 's|`pwd`|DIR|' /tmp/view3dscene_run_tests_output.txt
+REPOSITORY_DIR="`pwd`"
+REPOSITORY_DIR="`dirname \"$REPOSITORY_DIR\"`"
+sed --in-place=.bak2 -e "s|${REPOSITORY_DIR}|DIR|" /tmp/view3dscene_run_tests_output.txt
+
+# compare with last correct output
 diff -u /tmp/view3dscene_run_tests_output.txt jenkins_scripts/run_tests_valid_output.txt
 rm -f /tmp/view3dscene_run_tests_output.txt
