b28cfc80311bc0bfdba9b9eb3115581f3c921bf0
Michalis Kamburelis <michalis.kambi@gmail.com>
2020-12-04 21:41:03 +0100

    Add missing copyright

--- code/v3dsceneantialiasing.pas
+++ code/v3dsceneantialiasing.pas
@@ -1,3 +1,26 @@
+{
+  Copyright 2018-2020 Michalis Kamburelis.
+
+  This file is part of "view3dscene".
+
+  "view3dscene" is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  "view3dscene" is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with "view3dscene"; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+
+  ----------------------------------------------------------------------------
+}
+
+{ Anti-aliasing UI. }
 unit V3DSceneAntiAliasing;
 
 interface
