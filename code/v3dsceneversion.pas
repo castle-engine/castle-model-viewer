962af12e891e73b3d08cb193fbe6eb3f22ec6d26
Michalis Kamburelis <michalis.kambi@gmail.com>
2021-02-06 00:36:26 +0100

    Bump version to 4.0.0

--- code/v3dsceneversion.pas
+++ code/v3dsceneversion.pas
@@ -2,7 +2,7 @@
 unit V3DSceneVersion;
 interface
 const
-  Version = '3.19.0';
+  Version = '4.0.0';
   View3dsceneURL = 'https://castle-engine.io/view3dscene.php';
   SupportURL = 'https://castle-engine.io/donate.php';
 implementation
