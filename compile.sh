1f3881964ee5f79d773480acb952a4557f904c2f
Michalis Kamburelis <michalis.kambi@gmail.com>
2021-05-09 03:06:45 +0200

    Comment about order

--- compile.sh
+++ compile.sh
@@ -8,5 +8,5 @@ set -eu
 # build tovrmlx3d
 castle-engine compile ${CASTLE_ENGINE_TOOL_OPTIONS:-} --manifest-name=CastleEngineManifest.tovrmlx3d.xml
 
-# build view3dscene
+# build view3dscene (after tovrmlx3d, so that on Windows we have all DLLs necessary for view3dscene)
 castle-engine compile ${CASTLE_ENGINE_TOOL_OPTIONS:-}
