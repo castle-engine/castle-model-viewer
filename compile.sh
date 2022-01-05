3044610d0a62ac62d76bffa897778e7aa2504433
Michalis Kamburelis <michalis.kambi@gmail.com>
2022-01-05 01:13:17 +0100

    Updated CGE links, confgure->configure typo fix

--- compile.sh
+++ compile.sh
@@ -3,7 +3,7 @@ set -eu
 
 # Compile view3dscene and tovrmlx3d.
 # Compile using our build tool "castle-engine",
-# see https://github.com/castle-engine/castle-engine/wiki/Build-Tool.
+# see https://castle-engine.io/build_tool.
 
 # build tovrmlx3d
 castle-engine compile ${CASTLE_ENGINE_TOOL_OPTIONS:-} --manifest-name=CastleEngineManifest.tovrmlx3d.xml
