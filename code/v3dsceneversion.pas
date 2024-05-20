c70ff157572a830d342459fc201a0552348eaf40
Michalis Kamburelis <michalis.kambi@gmail.com>
2024-05-20 21:04:58 +0200

    Bump version to 5.0.0

--- code/v3dsceneversion.pas
+++ code/v3dsceneversion.pas
@@ -2,7 +2,7 @@
 unit V3DSceneVersion;
 interface
 const
-  Version = '4.3.0';
+  Version = '5.0.0';
   ViewerUrl = 'https://castle-engine.io/castle-model-viewer';
   SupportURL = 'https://www.patreon.com/castleengine';
 implementation
