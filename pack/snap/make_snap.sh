4d716454df7582f867f9217bf9a8e3778e8bc85f
Michalis Kamburelis <michalis.kambi@gmail.com>
2025-02-10 02:30:08 +0100

    Autogenerate snap recipe with proper version

--- pack/snap/make_snap.sh
+++ pack/snap/make_snap.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 set -eu
 
-# snapcraft warns about snapcraft.yaml~ otherwise.
-rm -f snap/*~
+rm -Rf snap/
+mkdir snap/
+# Old: snapcraft warns about snapcraft.yaml~ otherwise.
+# Not useful anymore, as we create clean snap/ directory.
+# rm -f snap/*~
+
+VERSION=`castle-engine output version`
+sed -e 's|${SNAP_VERSION}|'${VERSION}'|' snap/snapcraft.yaml.template
 
 snapcraft
 
 ls -Flah castle-model-viewer_*.snap
 
 echo 'To install run:'
-echo 'sudo snap install --devmode castle-model-viewer_4.1.0_amd64.snap'
+echo 'sudo snap install --devmode castle-model-viewer_*_amd64.snap'
